2b4a1f628ee7dede303efdb4022e22df7f5a8590
Samuel Petrocelli <spetroce@localhost.localdomain>
2016-07-20 00:10:05 -0400

    lots more bug fixes and refactoring

--- qt/advImageDisplay/advImageDisplay.h
+++ qt/advImageDisplay/advImageDisplay.h
@@ -58,21 +58,23 @@ class AdvImageDisplay : public QWidget{
   Q_OBJECT
   QGridLayout *layout_;
   QLabel *label_;
-  QImage qt_disp_img_;
-  cv::Mat cv_disp_img_;
+  QImage qt_src_img_;
+  cv::Mat src_img_;
+  std::mutex src_img_mtx_;
   bool is_init_;
   float resize_fx_total_, resize_fy_total_;
   cv::Size prev_src_img_size_;
-  std::mutex resize_total_mutex_;
+  std::mutex resize_total_mtx_;
   bool normalize_img_, convert_to_false_colors_, auto_convert_img_;
+  int id_;
+  bool show_image_, limit_view_;
 
   void UpdateDisplay();
-  void UpdateZoom(zoomInfo_t &zoom_info);
   void ResetResizeTotal();
 
   Roi roi_data_, disp_roi_;
   std::mutex roi_mask_mtx_;
-  cv::Mat roi_mask_, disp_roi_mask_, disp_roi_mask_resize_;
+  cv::Mat roi_mask_, disp_roi_mask_;
   //std::vector<Roi> roi_vec_;
   //std::vector< std::vector<cv::Point> > roi_polygons_tmp_;
   bool create_roi_, normalize_roi_, show_roi_;
@@ -85,19 +87,15 @@ class AdvImageDisplay : public QWidget{
   cv::Point2f Image2View(const cv::Point2f &img_coord);
   void Image2View(const std::vector<cv::Point2f> &src, std::vector<cv::Point> &dst, const bool scale_vertices);
 
-  zoomInfo_t zoom_info_, zoom_info_tmp_;
-  cv::Mat zoom_img_, final_img_;
+  int scroll_wheel_count_;
+  cv::Point2f pixmap_mouse_pos_;
+  cv::Mat zoom_img_, disp_img_;
   float zoom_scaler_, prev_zoom_, max_dim_scale_inv_;
   cv::Point2f origin_, origin_bounded_;
   cv::Size2f zoom_region_size_;
   bool is_zoom_;
   void UpdateZoom();
   void ResetZoom();
-
-  protected:
-    int id_;
-    std::mutex cv_disp_img_mtx_;
-    bool show_image_, limit_view_;
 
   public:
     AdvImageDisplay(QWidget *parent = NULL);
@@ -114,7 +112,7 @@ class AdvImageDisplay : public QWidget{
     //void ClearRoi();
     void RemoveRoi();
     void ShowStripes();
-    void ShowRoi(const bool state);
+    void ShowRoi();
     void SetNormalizeImage(const bool state);
     bool GetNormalizeImage();
     void SetNormalizeRoi(const bool state);
