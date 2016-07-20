31c933eda7148c6176e3bac3aea507f7614c53bf
Sam Petrocelli <sam.petrocelli@ge.com>
2016-07-20 10:08:50 -0400

    more code organization changes

--- qt/advImageDisplay/advImageDisplay.h
+++ qt/advImageDisplay/advImageDisplay.h
@@ -33,7 +33,6 @@
 
 
 struct Roi{
-  //enum Type{RECT = 0, POLY, CENTER_CIRCLE, DRAG_CIRCLE};
   static const int ROI_RECT = 0,
                    ROI_POLY = 1,
                    ROI_CENTER_CIRCLE = 2,
@@ -48,54 +47,8 @@ struct Roi{
 };
 
 
-struct SBackEndConvert{
-  bool perform;
-  float scale;
-};
-
-
 class AdvImageDisplay : public QWidget{
   Q_OBJECT
-  QGridLayout *layout_;
-  QLabel *label_;
-  QImage qt_src_img_;
-  cv::Mat src_img_;
-  std::mutex src_img_mtx_;
-  bool is_init_;
-  float resize_fx_total_, resize_fy_total_;
-  cv::Size prev_src_img_size_;
-  std::mutex resize_total_mtx_;
-  bool normalize_img_, convert_to_false_colors_, auto_convert_img_;
-  int id_;
-  bool show_image_, limit_view_;
-
-  void UpdateDisplay();
-  void ResetResizeTotal();
-
-  Roi roi_data_, disp_roi_;
-  std::mutex roi_mask_mtx_;
-  cv::Mat roi_mask_, disp_roi_mask_;
-  //std::vector<Roi> roi_vec_;
-  //std::vector< std::vector<cv::Point> > roi_polygons_tmp_;
-  bool create_roi_, normalize_roi_, show_roi_;
-  void DrawRoi(cv::Mat &img);
-  void CreateRoiMask();
-  void InitCreateRoi();
-  void UpdateRoiMask();
-
-  cv::Point2f View2Image(const cv::Point2f &view_coord);
-  cv::Point2f Image2View(const cv::Point2f &img_coord);
-  void Image2View(const std::vector<cv::Point2f> &src, std::vector<cv::Point> &dst, const bool scale_vertices);
-
-  int scroll_wheel_count_;
-  cv::Point2f pixmap_mouse_pos_;
-  cv::Mat zoom_img_, disp_img_;
-  float zoom_scaler_, prev_zoom_, max_dim_scale_inv_;
-  cv::Point2f origin_, origin_bounded_;
-  cv::Size2f zoom_region_size_;
-  bool is_zoom_;
-  void UpdateZoom();
-  void ResetZoom();
 
   public:
     AdvImageDisplay(QWidget *parent = NULL);
@@ -130,6 +83,43 @@ class AdvImageDisplay : public QWidget{
 
   protected:
     bool eventFilter(QObject *target, QEvent *event);
+
+  private:
+    bool is_init_, show_image_, limit_view_, normalize_img_, convert_to_false_colors_, auto_convert_img_;
+    int id_;
+    std::mutex src_img_mtx_;
+    QGridLayout *layout_;
+    QLabel *label_;
+    QImage qt_src_img_;
+    cv::Mat src_img_, disp_img_;
+    cv::Size prev_src_img_size_;
+
+    void UpdateDisplay();
+    cv::Point2f View2Image(const cv::Point2f &view_coord);
+    cv::Point2f Image2View(const cv::Point2f &img_coord);
+    void Image2View(const std::vector<cv::Point2f> &src, std::vector<cv::Point> &dst, const bool scale_vertices);
+
+    bool create_roi_, normalize_roi_, show_roi_;
+    float resize_fx_total_, resize_fy_total_;
+    Roi src_roi_, disp_roi_;
+    std::mutex resize_total_mtx_, roi_mask_mtx_;
+    //std::vector<Roi> roi_vec_;
+    //std::vector< std::vector<cv::Point> > roi_polygons_tmp_;
+    cv::Mat roi_mask_, disp_roi_mask_;
+    void DrawRoi(cv::Mat &img);
+    void CreateRoiMask();
+    void InitCreateRoi();
+    void UpdateRoiMask();
+    void ResetResizeTotal();
+
+    bool is_zoom_;
+    int scroll_wheel_count_;
+    float zoom_scaler_, prev_zoom_, max_dim_scale_inv_;
+    cv::Mat zoom_img_;
+    cv::Point2f origin_, origin_bounded_, pixmap_mouse_pos_;
+    cv::Size2f zoom_region_size_;
+    void UpdateZoom();
+    void ResetZoom();
 };
 
 #endif //__MIO_ADV_IMAGE_DISPLAY_H__
