ef9dbdac7ca2644b849c22294fc1816a4af1a21b
Sam Petrocelli <sam.petrocelli@ge.com>
2016-07-21 23:50:21 -0400

    updates

--- CMakeModules/FindQwt.cmake
+++ CMakeModules/FindQwt.cmake
@@ -6,12 +6,11 @@
 #  QWT_DEFINITIONS - Compiler switches required for using Qwt
 
 find_package(PkgConfig)
-pkg_check_modules(PC_QWT QUIET qwt)
+pkg_check_modules(PC_QWT QUIET Qt5Qwt6)
 set(QWT_DEFINITIONS ${PC_QWT_CFLAGS_OTHER})
 
-find_path(QWT_INCLUDE_DIR qwt/qwt.h
-          HINTS ${PC_QWT_INCLUDEDIR} ${PC_QWT_INCLUDE_DIRS}
-          PATH_SUFFIXES qwt)
+find_path(QWT_INCLUDE_DIR qwt_plot.h
+          HINTS ${PC_QWT_INCLUDEDIR} ${PC_QWT_INCLUDE_DIRS})
 
 find_library(QWT_LIBRARY NAMES qwt libqwt
              HINTS ${PC_QWT_LIBDIR} ${PC_QWT_LIBRARY_DIRS})
@@ -22,7 +21,7 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Qwt DEFAULT_MSG
                                   QWT_LIBRARY QWT_INCLUDE_DIR)
 
-mark_as_advanced(QWT_INCLUDE_DIR QWT_LIBRARY)
+mark_as_advanced(QWT_INCLUDE_DIR QWT_LIBRARY )
 
-set(QWT_LIBRARIES ${QWT_LIBRARY})
-set(QWT_INCLUDE_DIRS ${QWT_INCLUDE_DIR})
+set(QWT_LIBRARIES ${QWT_LIBRARY} )
+set(QWT_INCLUDE_DIRS ${QWT_INCLUDE_DIR} )
