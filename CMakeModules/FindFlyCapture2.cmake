bacbf36683f6607be49b6dfb300f28d119055482
Samuel Petrocelli <sam.petrocelli@ge.com>
2016-08-09 23:40:47 -0400

    variable name fix

--- CMakeModules/FindFlyCapture2.cmake
+++ CMakeModules/FindFlyCapture2.cmake
@@ -1,28 +1,28 @@
 # - Try to find FlyCapture2
 # Once done this will define
-#  FLY_CAPTURE_2_FOUND - System has FlyCapture2
-#  FLY_CAPTURE_2_INCLUDE_DIRS - The FlyCapture2 include directories
-#  FLY_CAPTURE_2_LIBRARIES - The libraries needed to use FlyCapture2
-#  FLY_CAPTURE_2_DEFINITIONS - Compiler switches required for using FlyCapture2
+#  FLYCAPTURE2_FOUND - System has FlyCapture2
+#  FLYCAPTURE2_INCLUDE_DIRS - The FlyCapture2 include directories
+#  FLYCAPTURE2_LIBRARIES - The libraries needed to use FlyCapture2
+#  FLYCAPTURE2_DEFINITIONS - Compiler switches required for using FlyCapture2
 
 find_package(PkgConfig)
-pkg_check_modules(PC_FLY_CAPTURE_2 QUIET FlyCapture2)
-set(FLY_CAPTURE_2_DEFINITIONS ${PC_FLY_CAPTURE_2_CFLAGS_OTHER})
+pkg_check_modules(PC_FLYCAPTURE2 QUIET FlyCapture2)
+set(FLYCAPTURE2_DEFINITIONS ${PC_FLYCAPTURE2_CFLAGS_OTHER})
 
-find_path(FLY_CAPTURE_2_INCLUDE_DIR FlyCapture2.h
-          HINTS ${PC_FLY_CAPTURE_2_INCLUDEDIR} ${PC_FLY_CAPTURE_2_INCLUDE_DIRS}
+find_path(FLYCAPTURE2_INCLUDE_DIR FlyCapture2.h
+          HINTS ${PC_FLYCAPTURE2_INCLUDEDIR} ${PC_FLYCAPTURE2_INCLUDE_DIRS}
           PATH_SUFFIXES flycapture)
 
-find_library(FLY_CAPTURE_2_LIBRARY NAMES flycapture libflycapture
-             HINTS ${PC_FLY_CAPTURE_2_LIBDIR} ${PC_FLY_CAPTURE_2_LIBRARY_DIRS})
+find_library(FLYCAPTURE2_LIBRARY NAMES flycapture libflycapture
+             HINTS ${PC_FLYCAPTURE2_LIBDIR} ${PC_FLYCAPTURE2_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set FLY_CAPTURE_2_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set FLYCAPTURE2_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(FlyCapture2 DEFAULT_MSG
-                                  FLY_CAPTURE_2_LIBRARY FLY_CAPTURE_2_INCLUDE_DIR)
+                                  FLYCAPTURE2_LIBRARY FLYCAPTURE2_INCLUDE_DIR)
 
-mark_as_advanced(FLY_CAPTURE_2_INCLUDE_DIR FLY_CAPTURE_2_LIBRARY )
+mark_as_advanced(FLYCAPTURE2_INCLUDE_DIR FLYCAPTURE2_LIBRARY )
 
-set(FLY_CAPTURE_2_LIBRARIES ${FLY_CAPTURE_2_LIBRARY} )
-set(FLY_CAPTURE_2_INCLUDE_DIRS ${FLY_CAPTURE_2_INCLUDE_DIR} )
+set(FLYCAPTURE2_LIBRARIES ${FLYCAPTURE2_LIBRARY} )
+set(FLYCAPTURE2_INCLUDE_DIRS ${FLYCAPTURE2_INCLUDE_DIR} )
