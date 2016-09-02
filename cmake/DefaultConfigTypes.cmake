44c64784e355eeb9c0fcd085b58396ee706bef2a
Samuel Petrocelli <sam.petrocelli@ge.com>
2016-09-02 16:21:02 -0400

    added cmake folder and a file for setting up Release and Debug variables

---
+++ cmake/DefaultConfigTypes.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "Configs" FORCE)
+if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+  message(STATUS "CMAKE_BUILD_TYPE was not set. Giving default value: Release")
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY VALUE Release)
+endif()
+
+set(FOAM_CXX_FLAGS "-std=c++11" CACHE STRING "Foam default CXX flags" FORCE) #-pedantic
+set(CMAKE_CXX_FLAGS_RELEASE "${FOAM_CXX_FLAGS} -O2")
+set(CMAKE_C_FLAGS_RELEASE "-O2")
+set(CMAKE_CXX_FLAGS_DEBUG "${FOAM_CXX_FLAGS} -g")
+set(CMAKE_C_FLAGS_DEBUG "-g")
+# Handle some compile flag options
+option(DEBUG_FLAG_GLIBCXX_DEBUG "use gcc flag -D_GLIBCXX_DEBUG when CMAKE_BUILD_TYPE is Debug" OFF)
+if(DEBUG_FLAG_GLIBCXX_DEBUG)
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_GLIBCXX_DEBUG")
+endif()
+message(STATUS "C++ flags (Release):${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE}")
+message(STATUS "C++ flags (Debug):${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+message(STATUS "C flags (Release):${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_RELEASE}")
+message(STATUS "C flags (Debug):${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}")
