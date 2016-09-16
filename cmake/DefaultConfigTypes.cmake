13411813c64ca2108afdd5bb0f7f6154c93224c5
Samuel Petrocelli <sam.petrocelli@gmail.com>
2016-09-16 13:18:03 -0400

    added debug options

--- cmake/DefaultConfigTypes.cmake
+++ cmake/DefaultConfigTypes.cmake
@@ -4,10 +4,10 @@ if("${CMAKE_BUILD_TYPE}" STREQUAL "")
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY VALUE Release)
 endif()
 
-set(CMAKE_CXX_FLAGS_RELEASE "-std=c++11 -pedantic -O2")
-set(CMAKE_C_FLAGS_RELEASE "-O2")
-set(CMAKE_CXX_FLAGS_DEBUG "-std=c++11 -pedantic -g")
-set(CMAKE_C_FLAGS_DEBUG "-g")
+set(CMAKE_CXX_FLAGS_RELEASE "-std=c++11 -pedantic -O2 -DNDEBUG")
+set(CMAKE_C_FLAGS_RELEASE "-O2 -DNDEBUG")
+set(CMAKE_CXX_FLAGS_DEBUG "-std=c++11 -pedantic -g  -O0 -DDEBUG -D_DEBUG")
+set(CMAKE_C_FLAGS_DEBUG "-g -O0 -DDEBUG -D_DEBUG")
 # Handle some compile flag options
 option(DEBUG_FLAG_GLIBCXX_DEBUG "use gcc flag -D_GLIBCXX_DEBUG when CMAKE_BUILD_TYPE is Debug" OFF)
 if(DEBUG_FLAG_GLIBCXX_DEBUG)
@@ -17,3 +17,4 @@ message(STATUS "C++ flags (Release):${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE
 message(STATUS "C++ flags (Debug):${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
 message(STATUS "C flags (Release):${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_RELEASE}")
 message(STATUS "C flags (Debug):${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}")
+
