468bcffcdc1ab13589e85309c09df1af6ef24b32
Sam Petrocelli <sam.petrocelli@gmail.com>
2017-09-14 20:15:32 -0400

    added path for local Qt install. added MIO_INCLUDE_DIR def.

--- qt/imageListViewer/PkgConfigPath.cmake
+++ qt/imageListViewer/PkgConfigPath.cmake
@@ -1,9 +1,19 @@
 set(SYSTEM_DETECTED ON)
 if(UNIX AND NOT APPLE)
   set(CODE_PREFIX_ "/home/$ENV{USER}")
+  set(Qt5_DIR_LINUX "/home/$ENV{USER}/Qt/5.9.1/gcc_64/lib/cmake/Qt5")
+  if(EXISTS ${Qt5_DIR_LINUX})
+    set(Qt5_DIR ${Qt5_DIR_LINUX})
+    message(STATUS "Qt5_DIR: ${Qt5_DIR}")
+  endif()
 elseif(APPLE)
   set(CODE_PREFIX_ "/Users/$ENV{USER}")
-  set(Qt5_DIR "/Users/$ENV{USER}/Qt/5.7/clang_64/lib/cmake/Qt5")
+  set(Qt5_DIR_APPLE "/Users/$ENV{USER}/Qt/5.9.1/clang_64/lib/cmake/Qt5")
+  if(EXISTS ${Qt5_DIR_APPLE})
+    set(Qt5_DIR ${Qt5_DIR_APPLE})
+    message(STATUS "Qt5_DIR: ${Qt5_DIR}")
+  endif()
+
 else()
   set(SYSTEM_DETECTED OFF)
 endif()
@@ -13,6 +23,7 @@ if(SYSTEM_DETECTED)
 
   ## mio
   set(MIO_INCLUDE_DIR "${CODE_PREFIX_}/code/src")
+  add_definitions(-DMIO_INCLUDE_DIR="${MIO_INCLUDE_DIR}")
 
   ## OpenCV
   set(OpenCV_DIR "${CODE_PREFIX_}/code/install/opencv/dev/rel/share/OpenCV")
