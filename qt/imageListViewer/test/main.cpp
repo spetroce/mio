b949130c23c8f7d4ccbd01a28a385bc2618b169c
Sam Petrocelli <sam.petrocelli@gmail.com>
2017-09-21 17:02:15 -0400

    removed qt style print out

--- qt/imageListViewer/test/main.cpp
+++ qt/imageListViewer/test/main.cpp
@@ -6,7 +6,6 @@
 
 int main(int argc, char *argv[]){
   QApplication a(argc, argv);
-  qDebug() << QStyleFactory::keys();
   QApplication::setStyle(QStyleFactory::create("Fusion"));
   mio::ImageListViewer w(true);
 
