72d1546cc0e8e2571b777771073b8b236d16ef3c
Sam Petrocelli <sam.petrocelli@gmail.com>
2019-09-27 14:17:24 -0400

    added new shmem test

--- ipc/test/shared_mem_test.cpp
+++ ipc/test/shared_mem_test.cpp
@@ -11,23 +11,25 @@ int main(int argc, char *argv[]){
   mio::Semaphore sem;
   sem.Init("/sem_test");
 
-  if(atoi(argv[1]) == 0){
-    for(;;){
+  if (atoi(argv[1]) == 0) {
+    // Producer
+    for (;;) {
       int num;
       std::cout << "enter a number\n";
       std::cin >> num;
       shmem.shm_addr_[0] = num;
       sem.Post(); //increment semaphore
-      if(num == 0)
+      if (num == 0)
         break;
     }
   }
-  else{
-    for(;;){
+  else {
+    // Consumer
+    for (;;) {
       sem.Wait(); //decrement semaphore
       int num = shmem.shm_addr_[0];
       std::cout << "shmem value: " << num << std::endl;
-      if(num == 0)
+      if (num == 0)
         break;
     }
   }
